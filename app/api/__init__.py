957391e1244572208b6fa2efb700857219f48a90
O.Tung <ongtung2212002@gmail.com>
2024-03-14 00:37:27 +0700

    refactor custom response

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from app.api.api_v1.endpoint import auth, user
 
-api_router = APIRouter()
+api_router = APIRouter(prefix="/v1/api")
 
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(user.router, prefix="/user", tags=["user"])
